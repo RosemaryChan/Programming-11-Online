65e52afcfce6a542a4f2c144d9201470356c5575
RosemaryChan <63425905+RosemaryChan@users.noreply.github.com>
2021-02-21 18:04:27 -0800

    Project Number one handin

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -2,18 +2,37 @@ public class Main {
     //Code your solution to problem number one here
     static int problemOne(String s){
         int answer = 0;
-        //your code here
+        int length = s.length();
+        for (int i=0; i <= length - 1;i++){
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+                answer++;
+            }
+        }
         return answer;
     }
     //Code you problem number two here
     static int problemTwo(String s){
         int answer = 0;
-        //your code here
+        int length = s.length();
+        for (int i=0; i <= length - 3;i++){
+            if(s.substring(i, i + 3).equals("bob")){
+                answer++;
+            }
+        }
         return answer;
     }
     //Code your solution to problem number 3 here
     static String problemThree(String s){
-        //your code here
+        int length = s.length();
+        for (int i=0; i <= length-1;i++){
+            for(int j=i+1;j<= length-1;j++){
+                if(s.charAt(i)<=s.charAt(j)){
+                    int x = i;
+                    int y = j;
+                    s= s.substring(x,y);//I couldn't solve this one
+                }
+            }
+        }
         return s;
     }
     public static void main(String[] args) {
